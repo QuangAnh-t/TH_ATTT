e9e1c3f2b8617e856a9d7f1033ea8b9ac0b86ca6
QUANGANHTRAN <tqayoutobe@gmail.com>
2024-10-27 17:41:57 +0700

    Update MahillActivity.java

--- app/src/main/java/com/example/th_attt/MahillActivity.java
+++ app/src/main/java/com/example/th_attt/MahillActivity.java
@@ -15,10 +15,6 @@ public class MahillActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.mahill);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        
     }
 }
